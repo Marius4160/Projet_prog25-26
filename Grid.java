19ec1376da0305af89969b96671af34d48e22e91
Marius4160 <mar.geoffrion12@gmail.com>
2025-12-18 14:18:40 +0100

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -1,80 +1,64 @@
-
 public class Grid {
 
-    private TileType[][] grid;
-    private int rows;
-    private int cols;
+    private final TileType [][] tiles;
+    private int rows, cols;
 
-    public Grid(int rows, int cols) {
-        this.rows = rows;
-        this.cols = cols;
-        this.grid = new TileType[rows][cols];
+    public Grid (TileType [][] tiles){
+        this.tiles = tiles;
+        this.rows= tiles.length;
+        this.cols = tiles[0].length;
+    }
+    public int rows() { return rows; }
+    public int cols() { return cols; }
+
+    public boolean isInside(Player.Position p) {
+        return p.row() >= 0 && p.row() < rows &&
+               p.col() >= 0 && p.col() < cols;
     }
 
-    public Grid(TileType[][] t) {
-        this.grid = t;
-    }
-
-    public void affichage() {
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                switch (grid[i][j]) {
-
-                    case Mur ->
-                        System.out.print("#");
-
-                    case BlocDestructible ->
-                        System.out.print("X");
-
-                    case SolVide ->
-                        System.out.print(" ");
-
-                    case Joueur ->
-                        System.out.print("@");
-
-                    case Objectif ->
-                        System.out.print(".");
-
-                    case BlocLeger ->
-                        System.out.print("o");
-
-                    case BlocGlissant ->
-                        System.out.print("O");
-
-                    case BlocEnergivore ->
-                        System.out.print("*");
-
-                    case PileEnergie ->
-                        System.out.print("+");
-
-                    case Explosif ->
-                        System.out.print("E");
-
-                    default ->
-                        System.out.print("?");
-                }
-            }
-            System.out.println();
+    public static boolean estBloc(TileType[][]tile, Player.Position p){
+        if(tile[p.row()][p.col()]==TileType.BlocDestructible){
+            return true;
+        }else if(tile[p.row()][p.col()]==TileType.BlocEnergivore){
+            return true;
+        }else if(tile[p.row()][p.col()]==TileType.BlocGlissant){
+            return true;
+        }else if(tile[p.row()][p.col()]==TileType.BlocLeger){
+            return true;
         }
+        return false;
+    }
+    
+    public TileType typeBloc(TileType[][]tile ,Player.Position p){
+        if(isInside(p)){
+            if(tile [p.row()][p.col()] == TileType.BlocDestructible){
+                return TileType.BlocDestructible;
+            }else if(tile [p.row()][p.col()] == TileType.BlocGlissant){
+                return TileType.BlocGlissant;
+            }else if(tile [p.row()][p.col()] == TileType.BlocEnergivore){
+                return TileType.BlocEnergivore;
+            }else if(tile [p.row()][p.col()] == TileType.BlocLeger){
+                return TileType.BlocLeger;
+            }
+        }
+        return null;
     }
     public boolean pousserBloc(Player.Position blocPos, Player.Direction dir) {
 
-        if (Grid.estBloc(grid, blocPos)==false) {
+        if (Grid.estBloc(tiles, blocPos)==false) {
             return false;
         }
         Player.Position nextPos = blocPos.translate(dir.dRow(), dir.dCol());
         if (isInside(nextPos)== false) {
             return false;
         }
-        if (grid[nextPos.row()][nextPos.col()] != TileType.SolVide) {
+        if (tiles[nextPos.row()][nextPos.col()] != TileType.SolVide) {
             return false;
         }
-        grid[nextPos.row()][nextPos.col()] = grid[blocPos.row()][blocPos.col()];
-        grid[blocPos.row()][blocPos.col()] = TileType.SolVide;
+        tiles[nextPos.row()][nextPos.col()] = tiles[blocPos.row()][blocPos.col()];
+        tiles[blocPos.row()][blocPos.col()] = TileType.SolVide;
 
-        
         return true;
     }
+
 }
-
-
