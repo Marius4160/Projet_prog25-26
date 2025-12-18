97ba174f341f87ae0f89025c147154610ba7fa01
Chlagibou <quentinberthonneau2610@gmail.com>
2025-12-18 13:47:58 +0100

    Implement Grid class with grid initialization and display

--- Grid.java
+++ Grid.java
@@ -1,3 +1,60 @@
+
 public class Grid {
-    
+
+    private TileType[][] grid;
+    private int rows;
+    private int cols;
+
+    public Grid(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.grid = new TileType[rows][cols];
+    }
+
+    public Grid(TileType[][] t) {
+        this.grid = t;
+    }
+
+    public void affichage() {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                switch (grid[i][j]) {
+
+                    case Mur ->
+                        System.out.print("#");
+
+                    case BlocDestructible ->
+                        System.out.print("X");
+
+                    case SolVide ->
+                        System.out.print(" ");
+
+                    case Joueur ->
+                        System.out.print("@");
+
+                    case Objectif ->
+                        System.out.print(".");
+
+                    case BlocLeger ->
+                        System.out.print("o");
+
+                    case BlocGlissant ->
+                        System.out.print("O");
+
+                    case BlocEnergivore ->
+                        System.out.print("*");
+
+                    case PileEnergie ->
+                        System.out.print("+");
+
+                    case Explosif ->
+                        System.out.print("E");
+
+                    default ->
+                        System.out.print("?");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
