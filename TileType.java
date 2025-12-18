5e20b0795cea49ca18d0ddf2b880e2be39720464
Marius4160 <mar.geoffrion12@gmail.com>
2025-12-18 10:39:14 +0100

    Add files via upload

---
+++ TileType.java
@@ -0,0 +1,12 @@
+enum TileType{
+    Mur, 
+    BlocDestructible,
+    SolVide,
+    Joueur,
+    Objectif,
+    BlocLeger,
+    BlocGlissant,
+    BlocEnergivore,
+    PileEnergie,
+    Explosif
+}
