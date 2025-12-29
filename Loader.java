583edba5b206d69fc6af264a426b47379cc02019
Marius4160 <mar.geoffrion12@gmail.com>
2025-12-29 11:48:37 +0100

    Update Loader.java

--- Loader.java
+++ Loader.java
@@ -1,3 +1,9 @@
-public class Loader {
-    
+import com.google.gson.*;
+import java.nio.file.*;
+
+class Loader {
+    static Level load(String filename) throws Exception {
+    var json = Files.readString(Path.of(filename));
+    return new Gson().fromJson(json, Level.class);
+    }
 }
