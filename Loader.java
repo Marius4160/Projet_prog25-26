8cafbc76f84a059adef1e90d871ddcb5002f7c5a
Marius4160 <mar.geoffrion12@gmail.com>
2026-01-05 17:58:51 +0100

    Update Loader.java

--- Loader.java
+++ Loader.java
@@ -1,9 +1,30 @@
-import com.google.gson.*;
-import java.nio.file.*;
+import com.google.*;
+import com.google.gson.Gson;
 
-class Loader {
-    static Level load(String filename) throws Exception {
-    var json = Files.readString(Path.of(filename));
-    return new Gson().fromJson(json, Level.class);
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Loader {
+
+    public static Level load(String fileName) throws Exception {
+        String json = Files.readString(Path.of(fileName));
+        return new Gson().fromJson(json, Level.class);
     }
+    /*public static void main(String[] args) {
+        Gson gson = new Gson();
+        try {
+            FileReader reader = new FileReader("level/leveldemo.json");
+            Level p = gson.fromJson(reader, Level.class);
+
+            System.out.println("Nom " + p.nom);
+            System.out.println("EnergieInitiale  "+p.energieInitiale);
+            System.out.println("Grille ");
+            for (String ligne : p.grille) {
+                System.out.println(ligne);
+            }
+        } catch (Exception e) {
+            e.fillInStackTrace();
+        }
+    }*/
 }
