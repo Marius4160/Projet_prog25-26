742d9ffe03f183b92c6c2fcc665491b876354362
Chlagibou <quentinberthonneau2610@gmail.com>
2026-01-05 10:31:38 +0100

    Enhance TileType with symbol conversion methods
    Added methods to convert TileType to and from symbols.

--- TileType.java
+++ TileType.java
@@ -1,5 +1,6 @@
-enum TileType{
-    Mur, 
+
+public enum TileType {
+    Mur,
     BlocDestructible,
     SolVide,
     Joueur,
@@ -8,5 +9,57 @@ enum TileType{
     BlocGlissant,
     BlocEnergivore,
     PileEnergie,
-    Explosif
+    Explosif;
+
+    public String toSymbol() {
+        return switch (this) {
+            case Mur ->
+                "#";
+            case BlocDestructible ->
+                "X";
+            case SolVide ->
+                " ";
+            case Joueur ->
+                "@";
+            case Objectif ->
+                ".";
+            case BlocLeger ->
+                "o";
+            case BlocGlissant ->
+                "O";
+            case BlocEnergivore ->
+                "*";
+            case PileEnergie ->
+                "+";
+            case Explosif ->
+                "E";
+        };
+    }
+
+    public static TileType fromSymbol(char symbol) {
+        return switch (symbol) {
+            case '#' ->
+                Mur;
+            case 'X' ->
+                BlocDestructible;
+            case ' ' ->
+                SolVide;
+            case '@' ->
+                Joueur;
+            case '.' ->
+                Objectif;
+            case 'o' ->
+                BlocLeger;
+            case 'O' ->
+                BlocGlissant;
+            case '*' ->
+                BlocEnergivore;
+            case '+' ->
+                PileEnergie;
+            case 'E' ->
+                Explosif;
+            default ->
+                throw new IllegalArgumentException("Symbole inconnu: " + symbol);
+        };
+    }
 }
