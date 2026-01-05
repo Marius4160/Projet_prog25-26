d04a8fb34eaacb6da6d507052fdb0f436562711f
Chlagibou <quentinberthonneau2610@gmail.com>
2026-01-05 14:45:30 +0100

    Update EtatJeu.java

--- EtatJeu.java
+++ EtatJeu.java
@@ -1,16 +1,13 @@
 
-import java.util.ArrayList;
 import java.util.List;
 
+public record EtatJeu(TileType[][] grid, Player.Position playerPos, List<Player.Position> blocs, int energie, int bombes, List<String> actions) {
 
-public record EtatJeu(TileType[][] grid,Player.Position playerPos,List<Player.Position> blocs,int energie,int bombes,List<String> actions) {
-
-    public EtatJeu {
+    public EtatJeu      {
         grid = copieGrid(grid);
         blocs = List.copyOf(blocs);
         actions = List.copyOf(actions);
     }
-
 
     private static TileType[][] copieGrid(TileType[][] grid) {
         TileType[][] copie = new TileType[grid.length][grid[0].length];
@@ -20,13 +17,12 @@ public record EtatJeu(TileType[][] grid,Player.Position playerPos,List<Player.Po
         return copie;
     }
 
-
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
             return true;
         }
-        if (!(obj instanceof EtatJeu other)){
+        if (!(obj instanceof EtatJeu other)) {
             return false;
         }
         if (energie != other.energie) {
@@ -65,43 +61,4 @@ public record EtatJeu(TileType[][] grid,Player.Position playerPos,List<Player.Po
         }
         return result;
     }
-
-
-    public EtatJeu ajouterAction(String action) {
-        List<String> newActions = new ArrayList<>(actions);
-        newActions.add(action);
-        return new EtatJeu(grid, playerPos, blocs, energie, bombes, newActions);
-    }
-
-    public EtatJeu consommerBombe() {
-        if (bombes <= 0) return null;
-        return new EtatJeu(grid, playerPos, blocs, energie, bombes - 1, actions);
-    }
-    public EtatJeu AjouterEnergie(int ajout) {
-        return new EtatJeu(grid, playerPos, blocs, energie+ajout, bombes, actions);
-    }
-
-    public EtatJeu ConsommerEnergie(int consommation) {
-        return new EtatJeu(grid, playerPos, blocs, energie-consommation, bombes, actions);
-    }
-    public EtatJeu deplacerBloc(Player.Position ancienne, Player.Position nouvelle) {
-        List<Player.Position> newBlocs = new ArrayList<>(blocs);
-        newBlocs.remove(ancienne);
-        newBlocs.add(nouvelle);
-        // consomme 1 d'energie pour bloc leger, 3 pour energivore, 0 pour glissant
-        TileType typeBloc = grid[ancienne.row()][ancienne.col()];
-        int coutEnergie = switch (typeBloc) {
-            case BlocLeger ->
-                1;
-            case BlocEnergivore ->
-                3;
-            case BlocGlissant ->
-                0;
-            default ->
-                0;
-        };
-        return new EtatJeu(grid, playerPos, newBlocs, energie - coutEnergie, bombes, actions);
-    }
-
 }
-
