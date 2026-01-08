4658bc0328edff2f2b348595547f1199d9ef1088
Marius4160 <mar.geoffrion12@gmail.com>
2026-01-08 14:41:19 +0100

    Update EtatJeu.java

--- EtatJeu.java
+++ EtatJeu.java
@@ -9,7 +9,7 @@ public record EtatJeu(TileType[][] grid, Player.Position playerPos, List<Player.
         actions = List.copyOf(actions);
     }
 
-    private static TileType[][] copieGrid(TileType[][] grid) {
+    private static TileType[][] copieGrid(TileType[][] grid) {  // clone la grille pour ne pas faire de modification direct et pouvoir faire les tests equals et hashcode
         TileType[][] copie = new TileType[grid.length][grid[0].length];
         for (int i = 0; i < grid.length; i++) {
             copie[i] = grid[i].clone();
@@ -17,24 +17,24 @@ public record EtatJeu(TileType[][] grid, Player.Position playerPos, List<Player.
         return copie;
     }
 
-    @Override
-    public boolean equals(Object obj) {
+    @Override 
+    public boolean equals(Object obj) { // réecrit le equals initialiser par le record EtatJeu
         if (this == obj) {
             return true;
         }
-        if (!(obj instanceof EtatJeu other)) {
+        if (!(obj instanceof EtatJeu other)) { // test si l'objet passé en parametre est un etat EtatJeu et si il est le meme que other <=> EtatJeu other = (EtatJeu) obj; --> cast
             return false;
         }
-        if (energie != other.energie) {
+        if (energie != other.energie) { //test si l'energie du record est la meme que celle de celle avec laquelle elle est comparée
             return false;
         }
-        if (bombes != other.bombes) {
+        if (bombes != other.bombes) {//test si le nombre de bombe du record est le meme que celui avec lequel il est comparé
             return false;
         }
-        if (!playerPos.equals(other.playerPos)) {
+        if (!playerPos.equals(other.playerPos)) { //test si la position du Player du record est la meme que celle avec laquelle elle est comparée
             return false;
         }
-        if (!blocs.equals(other.blocs)) {
+        if (!blocs.equals(other.blocs)) { // test si la liste des blocs du record est la meme que celle avec laquelle elle est comparée
             return false;
         }
         for (int i = 0; i < grid.length; i++) {
@@ -48,11 +48,11 @@ public record EtatJeu(TileType[][] grid, Player.Position playerPos, List<Player.
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode() { //reecrit le hashcode initial du record EtatJeu
         int result = energie;
-        result = 31 * result + bombes;
-        result = 31 * result + playerPos.hashCode();
-        result = 31 * result + blocs.hashCode();
+        result = 31 * result + bombes; //test pour chaque etat si il a déja été visité en 
+        result = 31 * result + playerPos.hashCode(); //ajoutant à la varaible résulte le nombre de bombes 
+        result = 31 * result + blocs.hashCode(); //mais aussi la valeur renvoyée par le hashCode de la position joueur et blocs
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
