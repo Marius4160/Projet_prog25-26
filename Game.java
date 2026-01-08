73318800bfe066dea4bd933d217375999fa90afb
Marius4160 <mar.geoffrion12@gmail.com>
2026-01-08 17:21:52 +0100

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
 
 public class Game {
-
     public static void main(String[] args) {
         Solver.loop();
+        boolean test = true;
+        while(test == true){
+            System.out.println("Voulez vous rejouer(Oui: Y/Non : N)");
+            Scanner sc =  new Scanner(System.in);
+            String input = sc.nextLine();
+            if (input.equals("Y")){
+                Solver.loop();
+            }
+            else if (input.equals("N")){
+                System.out.println("Fin du jeu");
+                test = false;
+            }else{
+                break;
+            }
+        sc.close();
+        }
+       
     }
+
 }
